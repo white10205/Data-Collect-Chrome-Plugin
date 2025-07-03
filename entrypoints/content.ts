2032608d87806c8abb487cfc1134ccffe98a7ac7
edy <edy@192.168.0.108>
2025-07-03 10:14:56 +0800

    feat: 再次保存一下

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -2,13 +2,13 @@ import { injectXHRAndFetchInterceptor } from '@/utils/injectScript';
 import { showBtn } from '@/utils/showBtn';
 export default defineContentScript({
   matches: ['*://*.xiaohongshu.com/*'],
-  main() {
-
+  async main() {
     //注入脚本
     injectXHRAndFetchInterceptor();
 
     // 展示按钮
     showBtn();
+
 
     window.addEventListener('message', (event) => {
       if (
@@ -16,15 +16,12 @@ export default defineContentScript({
         event.data &&
         event.data.source === 'inject-xhs'
       ) {
-
         // 发送到 background
         browser.runtime.sendMessage({
           type: 'XHS_NOTE_RESULT',
           data: event.data.payload,
         });
-
       }
     });
-    
   },
 });
